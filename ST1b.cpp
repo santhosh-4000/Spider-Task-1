9d3f5521541d7e365ddc581c5b59dd2fc74cacbc
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2020-08-19 21:14:58 +0530

    Update ST1b.cpp
    comments added

--- ST1b.cpp
+++ ST1b.cpp
@@ -6,16 +6,21 @@ using namespace std;
 int symmetry(string str){
 	int n=str.size();
 	
+	//base case
 	if(n==1)
 	return 0;
 	
+	//copy first and second half of strings
 	string first,second;
 	first=str.substr(0,n/2);
 	second=str.substr(n/2,n/2);
 	
+	//if first and second halfs are equal
+	//increment result by one
 	if(first==second)
 	return symmetry(first)+1;
 	
+	//return 0 otherwise
 	return 0;
 }
 
@@ -26,5 +31,6 @@ int main(){
 	string str;
 	cin>>str;
 	
+	//calculate using recursive function
 	cout<<symmetry(str)<<endl;
 }
