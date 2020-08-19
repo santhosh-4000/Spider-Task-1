13fcafa6a160bdfc3b6f13e18b4f6e633fbb74fc
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2020-08-19 21:00:16 +0530

    Update ST1a.cpp
    comments added

--- ST1a.cpp
+++ ST1a.cpp
@@ -5,10 +5,13 @@
 using namespace std;
 
 string addOne(string str){
+	//add 1 to binary string str and return it
 	string res="";
 	int s=1;
+	//iterate over all bits in str
    int i=str.size()-1;
    while(i>=0){
+	   //add bits to s from right
    	s+=(str[i]-'0');
    	res=char(s%2+'0')+res;
    	s=s/2;
@@ -18,10 +21,14 @@ string addOne(string str){
 }
 
 string subOne(string str){
+	//subtract 1 from binary string str
 	string res="";
 	int s=1;
    int i=str.size()-1;
+	//itearate over all bits from right to left
    while(i>=0){
+	//toggle least significant 1 and change all 
+	//bits right to it to 1
    	if(s!=2){
    	s+=(str[i]-'0');
    	res=char(s%2+'0')+res;
@@ -36,12 +43,14 @@ string subOne(string str){
 
 
 int main(){
-	int len;
-	 
+	
+	//take inputs
+	int len; 
 	cin>>len;
 	string str;
-
 	cin>>str;
+	
+	//check if all are zeros or all are ones
 	int check=0;
 	for(int i=0;i<str.size();i++){
 		if(str[i]=='0')
@@ -49,6 +58,8 @@ int main(){
 	}
 	if(check==0||check==str.size())
 	cout<<"-1"<<endl;
+	
+	//print integers just one greater and one less than given number
 	else
 	cout<<subOne(str)<<" "<<addOne(str);
 }
